fd30c3efcb0b3871664332cd4e04bdb2776c8e76
yastil01 <63788022+yastil01@users.noreply.github.com>
2021-01-06 11:35:18 -0800

    Create LC_621_task_scheduler.py

---
+++ LC_621_task_scheduler.py
@@ -0,0 +1,18 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        if n == 0:
+            return len(tasks)
+    
+        freq = collections.Counter(tasks)
+        max_freq = max(freq.values())
+        freq = list(freq.values())
+        max_freq_ele_count = 0                 # total_elements_with_max_freq, last row elements
+        i = 0
+        while( i < len(freq)):
+            if freq[i] == max_freq:
+                max_freq_ele_count += 1
+            i += 1
+            
+        ans = (max_freq - 1) * (n+1) + max_freq_ele_count
+        
+        return max(ans, len(tasks))
